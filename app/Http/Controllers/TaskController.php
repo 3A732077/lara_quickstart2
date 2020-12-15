84dcc924994464abe8f580ba8650698ace51724d
yu-jie shen <rock.02130529@gmail.com>
2020-12-15 22:44:06 +0800

    驗證使用者身分

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function  _construct()
+    {
+        $this->middleware('auth');
+    }
+
+
 }
